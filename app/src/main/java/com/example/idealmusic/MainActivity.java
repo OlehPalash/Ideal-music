149896f52ec86cdf6b96bcf9c1064be46d84b85d
Yurii Shynkar <yurii.shynkar@stuzo.com>
2020-03-15 00:44:56 +0200

    refactoring

--- app/src/main/java/com/example/idealmusic/MainActivity.java
+++ app/src/main/java/com/example/idealmusic/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,7 +20,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.idealmusic.common.MediaPlayerHelper;
-import com.example.idealmusic.common.SeekBarThread;
+import com.example.idealmusic.model.Track;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -30,21 +31,23 @@ import com.karumi.dexter.listener.single.PermissionListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+
     ListView myListViewForSong; // создаем лист
     Button btn_next, btn_previous, btn_pause;
     TextView songTextLabel;
     SeekBar songSeekBar;
     LinearLayout playerControlBar;
 
-    SeekBarThread seekBarThread;
     String songTitleLabel;
-    private List<File> songsList = new ArrayList<File>();
+    private List<Track> songsList = new ArrayList<>();
     private int position = 0;
     private MediaPlayer player;
 
-    MediaPlayerHelper playerHelper;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //задаёт начальную установку параметров при инициализации активности
@@ -66,19 +69,18 @@ public class MainActivity extends AppCompatActivity {
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                position = ((position + 1) % songsList.size());
-                playSong(position);
+                next();
             }
         });
+
         btn_previous = findViewById(R.id.previous);
         btn_previous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                position = ((position + 1) < 0) ? (songsList.size() - 1) : (position - 1);
-                playSong(position);
-
+                previous();
             }
         });
+
         btn_pause = findViewById(R.id.pause);
         btn_pause.setBackgroundResource(R.drawable.icon_play);
         btn_pause.setOnClickListener(new View.OnClickListener() {
@@ -86,13 +88,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (player == null) return;
 
-                songSeekBar.setMax(player.getDuration());
                 if (player.isPlaying()) {
-                    btn_pause.setBackgroundResource(R.drawable.icon_play);
-                    player.pause();
+                    stop();
                 } else {
-                    btn_pause.setBackgroundResource(R.drawable.icon_pause);
-                    player.start();
+                    resume();
                 }
             }
         });
@@ -108,60 +107,99 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                player.seekTo(seekBar.getProgress());
+                if (player.getCurrentPosition() >= seekBar.getProgress()) {
+                    next();
+                } else {
+                    player.seekTo(seekBar.getProgress());
+                }
+                if (!player.isPlaying()) {
+                    player.start();
+                }
             }
         });
 
         lockUI(true);
-        playerHelper = MediaPlayerHelper.getInstance();
         checkPermission(); // Этот класс для полномочий времени выполнения
     }
 
-    void runPlayer() {
-        seekBarThread = new SeekBarThread(player, songSeekBar);
-        seekBarThread.start();
-        songsList = playerHelper.getTrackList(this);
-        if (!songsList.isEmpty()) {   // prepare play on load
-            songTitleLabel = songsList.get(0).getName();
-            songTextLabel.setText(songTitleLabel);
-            songSeekBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
-            songSeekBar.getThumb().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
-            songSeekBar.setProgress(0);
-            seekBarThread.setCurrentSeekBarPosition(0);
-            Uri u = Uri.parse(songsList.get(position).toString());  //Открытый конечный класс
-            player = MediaPlayer.create(this, u);
-
-            songSeekBar.setMax(player.getDuration()); // Максимальная продолжительность песни
-        }
-        ArrayAdapter myAdapter =
-                new ArrayAdapter<File>(this, android.R.layout.simple_list_item_1, songsList);
-        myListViewForSong.setAdapter(myAdapter);
-        myListViewForSong.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
-                playSong(i);
-            }
-        });
-        lockUI(false);
+    private void stop() {
+        player.stop();
+        updatePlayButton();
+        songSeekBar.setProgress(0);
     }
 
-    private void playSong(int position) {
-        if (player != null && !songsList.isEmpty()) {
-            songTitleLabel = songsList.get(position).getName();
+    private void previous() {
+        position = ((position - 1) < 0) ? (songsList.size() - 1) : (position - 1);
+        play(position);
+    }
+
+    private void next() {
+        position = ((position + 1) % songsList.size());
+        play(position);
+    }
+
+    private void resume() {
+        player.start();
+        updatePlayButton();
+    }
+
+    private void play(int position) {
+        if (player != null && !songsList.isEmpty() && position >= 0 && position < songsList.size()) {
+            Track track = songsList.get(position);
+            songTitleLabel = track.getTitle().isEmpty() ? track.getDescription().isEmpty() ? track.getFile().getName() : "" : "";
             songTextLabel.setText(songTitleLabel);
-            songSeekBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
-            songSeekBar.getThumb().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
             songSeekBar.setMax(player.getDuration()); // Максимальная продолжительность песни
             songSeekBar.setProgress(0);
-            seekBarThread.setCurrentSeekBarPosition(0);
             player.stop();
             player.release();
         }
-        btn_pause.setBackgroundResource(R.drawable.icon_pause);
 
-        Uri u = Uri.parse(songsList.get(position).toString());  //Открытый конечный класс
+        Uri u = Uri.parse(songsList.get(position).getFile().toString());  //Открытый конечный класс
         player = MediaPlayer.create(this, u);
         player.start();
+        updatePlayButton();
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if ((player != null) && (player.isPlaying())) {
+                    songSeekBar.setProgress(player.getCurrentPosition());
+                    mHandler.postDelayed(this, 1000);
+                }
+            }
+        });
+    }
+
+    void runPlayer() {
+        songsList = MediaPlayerHelper.getTrackList(this);
+        if (songsList.isEmpty()) {
+            lockUI(true);
+        } else {
+            lockUI(false);
+
+            songTitleLabel = songsList.get(0).getTitle();
+            songTextLabel.setText(songTitleLabel);
+
+            Uri u = Uri.parse(songsList.get(position).getFile().toString());  //Открытый конечный класс
+            player = MediaPlayer.create(this, u);
+            songSeekBar.setProgress(0);
+            songSeekBar.setMax(player.getDuration()); // Максимальная продолжительность песни
+            updatePlayButton();
+
+            List<String> trackName = new ArrayList<>();
+            for (int i = 0; i < songsList.size(); i++) {
+                Track f = songsList.get(i);
+                trackName.add(!f.getTitle().isEmpty() ? f.getTitle() : !f.getDescription().isEmpty() ? f.getDescription() : "Track " + i);
+            }
+
+            ArrayAdapter myAdapter = new ArrayAdapter<Track>(this, android.R.layout.simple_list_item_1, songsList);
+            myListViewForSong.setAdapter(myAdapter);
+            myListViewForSong.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+                    play(i);
+                }
+            });
+        }
     }
 
     private void lockUI(boolean lockUI) {
@@ -170,6 +208,19 @@ public class MainActivity extends AppCompatActivity {
         } else {
             playerControlBar.setVisibility(View.VISIBLE);
         }
+    }
+
+    private void updatePlayButton() {
+        int colorRes;
+        if (player.isPlaying()) {
+            btn_pause.setBackgroundResource(R.drawable.icon_pause);
+            colorRes = R.color.colorPrimary;
+        } else {
+            btn_pause.setBackgroundResource(R.drawable.icon_play);
+            colorRes = R.color.colorPrimaryDark;
+        }
+        songSeekBar.getProgressDrawable().setColorFilter(getResources().getColor(colorRes), PorterDuff.Mode.MULTIPLY);
+        songSeekBar.getThumb().setColorFilter(getResources().getColor(colorRes), PorterDuff.Mode.SRC_IN);
     }
 
     private void showMessage(String message) {
@@ -182,7 +233,7 @@ public class MainActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse response) {
-                        songsList = playerHelper.getTrackList(MainActivity.this);
+                        songsList = MediaPlayerHelper.getTrackList(MainActivity.this);
                         if (songsList.isEmpty()) {
                             lockUI(true);
                         } else {
--- app/src/main/java/com/example/idealmusic/common/MediaPlayerHelper.java
+++ app/src/main/java/com/example/idealmusic/common/MediaPlayerHelper.java
@@ -2,51 +2,29 @@ package com.example.idealmusic.common;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.net.Uri;
 import android.provider.MediaStore;
+
+import com.example.idealmusic.model.Track;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MediaPlayerHelper {
-    private Context context;
-    private static MediaPlayerHelper instance;
-    private android.media.MediaPlayer player;
 
-    private List songsList = new ArrayList<File>();
-
-    private MediaPlayerHelper() {
+    public static List<Track> getTrackList(Context context) {
+        return MediaPlayerHelper.getAllAudios(context);
     }
 
-    public static MediaPlayerHelper getInstance() {
-        if (instance == null) {
-            instance = new MediaPlayerHelper();
-        }
-        return instance;
-    }
-
-    public void playByPosition(Context context, int position) {
-        if (songsList.isEmpty()) return;
-
-        if (player != null) { // окончание трека
-            player.stop();
-            player.release();
-        }
-
-        Uri u = Uri.parse(songsList.get(position).toString()); //Открытый конечный класс
-        player = android.media.MediaPlayer.create(context, u);
-        player.start();
-    }
-
-    public List getTrackList(Context context) {
-        return getAllAudios(context);
-    }
-
-    private List<File> getAllAudios(Context context) { //функция, которая дает нам все аудиофайлы в File Object.
+    private static List<Track> getAllAudios(Context context) { //функция, которая дает нам все аудиофайлы в File Object.
+        List<Track> tracks = new ArrayList<>();
         List<File> files = new ArrayList<>();
+
         String[] projection = {MediaStore.Audio.AudioColumns.DATA, MediaStore.Audio.Media.DISPLAY_NAME};
         Cursor cursor = context.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
+        if (cursor == null) {
+            return tracks;
+        }
         try {
             cursor.moveToFirst();
             do {
@@ -57,22 +35,9 @@ public class MediaPlayerHelper {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return files;
-    }
-
-    public ArrayList<File> findSong(File file) {  //Делаем список масивов чтобы найти песню
-        ArrayList<File> arrayList = new ArrayList<>();
-        File[] files = file.listFiles();
-        for (File singleFile : files) { // Логічна функція
-            if (singleFile.isDirectory()/* && !singleFile.isHidden()*/) {
-                arrayList.addAll(findSong(singleFile));
-            } else {
-                if (singleFile.getName().endsWith(".mp3") ||
-                        singleFile.getName().endsWith(".m4a")) {
-                    arrayList.add(singleFile);
-                }
-            }
+        for (File file : files) {
+            tracks.add(new Track(file, file.getName(), file.getPath()));
         }
-        return arrayList;
+        return tracks;
     }
 }
